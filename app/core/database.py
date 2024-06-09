841000c70d71b966577916bf451153a8b555317a
Sizderly <garciachable0@gmail.com>
2024-06-08 18:38:11 -0600

    Cambiando de base de datos y modificando las estructuras de las tablas. Agregando roles y valoraciones

--- app/core/database.py
+++ app/core/database.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql://admin:123@localhost/pruebaLogIn"
+SQLALCHEMY_DATABASE_URL = "mysql://admin:123@localhost/gorthondev"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL,max_overflow=100)
 
 SessionLocal = sessionmaker(autocommit= False, autoflush=False, bind=engine)
 
--- app/models.py
+++ app/models.py
@@ -1,10 +1,38 @@
 from app.core.database import Base
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String,Date
+from sqlalchemy.orm import Relationship
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "user"
 
     id = Column(Integer, primary_key=True)
-    email = Column(String(1000), index=True)
+    email = Column(String(100), index=True)
     username = Column(String(20),index=True)
-    hashed_password = Column(String(1000))
+    password = Column(String(1000))
+    role_id = Column(Integer, ForeignKey("role.id"),nullable=False)
+
+    role = Relationship("Role", back_populates="user")
+    rating = Relationship("Rating",back_populates="user")
+
+
+
+
+class Role(Base):
+    __tablename__ = "role"
+
+    id = Column(Integer, primary_key=True)
+    role = Column(String(20), index=True)
+
+    user = Relationship("User",back_populates="role")
+
+
+class Rating(Base):
+    __tablename__ = "rating"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("user.id"))
+    rating = Column(Integer)
+    comment = Column(String(300))
+
+    user = Relationship("User",back_populates="rating")
+    
