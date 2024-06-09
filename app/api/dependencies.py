77cc5eb43dde5d676c691125be2949c52336b7d7
Sizderly <garciachable0@gmail.com>
2024-06-08 18:22:09 -0600

    Agregnado dependencia para autorizacion de token para administradores

--- app/api/dependencies.py
+++ app/api/dependencies.py
@@ -1,4 +1,4 @@
-from jwt.exceptions import InvalidTokenError
+from ..schemas import Role,User
 from fastapi import HTTPException,Depends
 from fastapi.security import OAuth2PasswordBearer
 from ..crud import get_user_by_username
@@ -6,6 +6,7 @@ from typing import Annotated
 from sqlalchemy.orm import Session
 from app.core import database
 from app.core.security import decode_token
+
 
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
@@ -25,16 +26,17 @@ SessionDb = Annotated[Session, Depends(get_db)]
 TokenDep = Annotated[str,Depends(oauth2_scheme)]
 
 
-async def get_current_user(session: SessionDb, token: TokenDep):
-    token_data = await decode_token(token=token)
+async def get_current_user(session: SessionDb, token: TokenDep) -> User:
+    token_data = decode_token(token=token)
     user = get_user_by_username(session, username=token_data.username)
     if user is None:
         raise HTTPException(status_code=400, detail="User not found")
     return user
 
-async def verify_token(token:TokenDep):
-    try:
-        token_data = decode_token(token=token)
-    except :
-        raise HTTPException(status_code=401)
-    return token_data
+CurrentUser = Annotated[str,Depends(get_current_user)]
+
+
+async def admin_authorization(current_user : CurrentUser) -> User:
+    if current_user.role_id != Role.Admin.value:
+        raise HTTPException(status_code=403, detail="No cuenta con los permisos suficientes para acceder al recurso")
+    return current_user
