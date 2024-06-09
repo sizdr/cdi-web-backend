a2f16c8ab327866300262cc1bffe19b2b1b997ce
Sizderly <garciachable0@gmail.com>
2024-06-08 18:23:51 -0600

    Cambio de nombre al atributo password del modelo de usuario

--- app/crud.py
+++ app/crud.py
@@ -1,18 +1,18 @@
 from sqlalchemy.orm import Session
 from app import models
-from app.schemas import User,UserCreate
+from app.schemas import User,UserCreate,UserDB
 from app.core import security
 
 
-def get_user_by_email(db:Session, email:str) -> User:
+def get_user_by_email(db:Session, email:str) -> UserDB:
     return db.query(models.User).filter(models.User.email == email).first()
 
-def get_user_by_username(db:Session, username: str) -> User:
+def get_user_by_username(db:Session, username: str) -> UserDB:
     return db.query(models.User).filter(models.User.username == username).first()
 
-def create_user(db: Session, user: UserCreate):
+def create_user(db: Session, user: UserCreate) -> User:
     hashed_password = security.get_password_hashed(user.password)
-    db_user = models.User(email=user.email, hashed_password=hashed_password,username=user.username)
+    db_user = models.User(email=user.email, password=hashed_password,username=user.username,role_id = user.role.value)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
@@ -25,7 +25,7 @@ def authenticate_user_by_username(db:Session, username:str, password:str) -> Use
     user = get_user_by_username(db,username)
     if not user:
         return False
-    if not security.verify_password(password,user.hashed_password):
+    if not security.verify_password(password,user.password):
         return False
     return user
 
@@ -33,7 +33,7 @@ def authenticate_user(db:Session, email:str, password:str) -> User:
     user = get_user_by_email(db,email)
     if not user:
         return False
-    if not security.verify_password(password,user.hashed_password):
+    if not security.verify_password(password,user.password):
         return False
     return user
 
